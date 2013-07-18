e2a27b7d264bad60bc7b55b0e08b33f6d13020cf
angrymarmot <angrymarmot@fastmail.net>
2013-07-17 20:55:27 -0700

    Final

--- tfa.py
+++ tfa.py
@@ -1,12 +1,7 @@
-from sys import argv
-from collections import defaultdict
- 
 logs = open("log.txt")
-roomMax, visitorMax, timeMax = 99, 1023, 1439
 rooms = {}
-vals = {}
 avgs = {}
-rmv = {}
+
 for line in logs:
     line = line.strip().split(" ")
     p, r, d, t = int(line[0]), int(line[1]), line[2], int(line[3])
@@ -14,24 +9,17 @@ for line in logs:
     if key not in rooms.keys():
         rooms[key] = (d, t)
     else:
-        vals[key] = t - rooms[key][1] + 1
-    if key[0] not in avgs.keys():
-        avgs[key[0]] = 1
-    else:
-        avgs[key[0]] += 1
- 
-for rm in vals.keys():
-    if rm[0] not in avgs.keys():
-        avgs[rm[0]] = 0
-        avgs[rm[0]] += vals[rm]
- 
-    else:
-        avgs[rm[0]] += vals[rm]
-print "raw"
-for k, v in rooms.items():
-    print k, v
-print "avgs"
-for a, b in vals.items():
-    print a, b
+        if r in avgs:
+            avgs[r] += [t - rooms[key][1] + 1]
+        else:
+            avgs[r] = [t - rooms[key][1] + 1]
 
- 
+for room in sorted(avgs.keys()):
+    if len(avgs[room]) > 1:
+        print "Room", room, "-", sum(avgs[room]) / len(avgs[room]), "minute average visit,", len(avgs[room]), "visitors total."
+    else:
+        print "Room", room, "-", sum(avgs[room]) / len(avgs[room]), "minute average visit,", len(avgs[room]), "visitor total."
+
+
+
+
